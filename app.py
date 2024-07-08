785a3a3d419c6acb3b979d4eedeee0b39d962682
Lovely-logesh <146011778+Lovely-logesh@users.noreply.github.com>
2024-07-08 00:38:29 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,10 @@ import streamlit as st
 import hashlib
 import numpy as np
 import pandas as pd
-#import matplotlib.pyplot as plt
 from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
-#from sklearn.preprocessing import StandardScaler
-import tensorflow as tf
 from tensorflow import keras
+from sklearn.preprocessing import StandardScaler
 
 # Hardcoded username and password hashes for demonstration
 USERS = {
