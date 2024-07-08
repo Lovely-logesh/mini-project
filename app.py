05d1173b460b23c2f872e79bc35f3d551a7676d4
Lovely-logesh <146011778+Lovely-logesh@users.noreply.github.com>
2024-07-07 23:58:12 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ import streamlit as st
 import hashlib
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
+#from sklearn.preprocessing import StandardScaler
 import tensorflow as tf
 from tensorflow import keras
 
